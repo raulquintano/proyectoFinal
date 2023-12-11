13c3f6328430a7650df217e2e6761637e2dc130d
raulquintano <raul.quintanovazquez@usp.ceu.es>
2023-12-11 16:15:44 +0100

    Codigo del Main y arreglo de la interfaz

---
+++ src/principal/Main.java
@@ -0,0 +1,11 @@
+package principal;
+import interfaz.*;
+
+public class Main {
+    public static void main(String[] args) 
+    {
+        Interfaz interfaz = new Interfaz();
+        interfaz.mostrarCatalogo();
+    }
+    
+}
